dd10ac8442c220e8643990dccc609db2e1216e61
Alberto Cebrián Medialdea <acebrian@paradigmadigital.com>
2016-11-10 16:52:06 +0100

    Controller and his test

--- src/main/java/com/opipo/relac/controller/RelationController.java
+++ src/main/java/com/opipo/relac/controller/RelationController.java
@@ -1,5 +1,59 @@
 package com.opipo.relac.controller;
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.opipo.relac.model.Relation;
+import com.opipo.relac.service.RelationService;
+import com.vividsolutions.jts.util.Assert;
+
+@RestController
+@RequestMapping("/character/{owner}/relationship/{otherCharacter}/relation")
 public class RelationController {
+
+	@Autowired
+	private RelationService relationService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<Date>> list(@PathVariable("owner") String ownersName,
+			@PathVariable("characterName") String otherName) {
+		return new ResponseEntity<List<Date>>(
+				relationService.list(ownersName, otherName).stream().map(f -> f.getDate()).collect(Collectors.toList()),
+				HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{date}", method = RequestMethod.GET)
+	public ResponseEntity<Relation> get(@PathVariable("owner") String ownersName,
+			@PathVariable("characterName") String otherName, @PathVariable("date") Date date) {
+		return new ResponseEntity<Relation>(relationService.get(ownersName, otherName, date), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{date}", method = RequestMethod.PUT)
+	public ResponseEntity save(@PathVariable("owner") String ownersName,
+			@PathVariable("characterName") String otherName, @PathVariable("date") Date date,
+			@RequestBody Relation relation) {
+		Assert.equals(date, relation.getDate(), "The date is not the expected");
+		relationService.save(ownersName, otherName, relation);
+		return new ResponseEntity(HttpStatus.ACCEPTED);
+	}
+
+	@RequestMapping(value = "/{date}", method = RequestMethod.POST)
+	public ResponseEntity create(@PathVariable("owner") String ownersName,
+			@PathVariable("characterName") String otherName, @PathVariable("date") Date date) {
+		Relation relation = new Relation();
+		relation.setDate(date);
+		relationService.save(ownersName, otherName, relation);
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
 
 }
--- src/test/java/com/opipo/relac/controller/RelationControllerTest.java
+++ src/test/java/com/opipo/relac/controller/RelationControllerTest.java
@@ -1,23 +1,28 @@
 package com.opipo.relac.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.opipo.relac.model.Character;
 import com.opipo.relac.model.Relation;
 import com.opipo.relac.service.RelationService;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RelationControllerTest {
 	@Mock
 	private RelationService relationService;
@@ -39,7 +44,6 @@ public class RelationControllerTest {
 
 		ResponseEntity<List<Date>> response = relationController.list(ownerName, otherName);
 
-
 		assertNotNull(response);
 		assertNotNull(response.getBody());
 		List<Date> dates = response.getBody();
@@ -49,16 +53,16 @@ public class RelationControllerTest {
 		assertEquals(relations.size(), dates.size());
 		assertEquals("HTTPCode isn't ok", response.getStatusCode(), HttpStatus.OK);
 	}
-	
+
 	@Test
-	public void givenOwnerIdAndOtherCharIdAndDateThenReturnIt(){
+	public void givenOwnerIdAndOtherCharIdAndDateThenReturnIt() {
 		String ownerName = "Owner Name";
 		String otherName = "Other name";
 		Date date = new Date();
 		Relation relation = new Relation();
 		relation.setDate(date);
 
-		Mockito.when(relationService.get(ownerName, otherName,date)).thenReturn(relation);
+		Mockito.when(relationService.get(ownerName, otherName, date)).thenReturn(relation);
 
 		ResponseEntity<Relation> response = relationController.get(ownerName, otherName, date);
 
@@ -66,46 +70,45 @@ public class RelationControllerTest {
 		assertNotNull(response.getBody());
 		assertEquals(date, response.getBody().getDate());
 		assertEquals("HTTPCode isn't ok", response.getStatusCode(), HttpStatus.OK);
-		
+
 	}
-	
+
 	@Test
-	public void givenOwnerIdAndOthercharIdAndDateThenCreateIt(){
+	public void givenOwnerIdAndOthercharIdAndDateThenCreateIt() {
 		String ownerName = "Owner Name";
 		String otherName = "Other name";
 		Date date = new Date();
 		Relation relation = new Relation();
 		relation.setDate(date);
 
-		
 		ArgumentCaptor<Relation> relationCaptor = ArgumentCaptor.forClass(Relation.class);
-		ResponseEntity response = relationController.create(ownerName,otherName,date);
-		
+		ResponseEntity response = relationController.create(ownerName, otherName, date);
+
 		Mockito.verify(relationService).save(Mockito.eq(ownerName), Mockito.eq(otherName), relationCaptor.capture());
-		
-		assertNotNull("Character isn't send to service",relationCaptor.getValue());
+
+		assertNotNull("Character isn't send to service", relationCaptor.getValue());
 		assertEquals("The date is not the expected", date, relationCaptor.getValue().getDate());
-		assertNotNull("Response is null",response);
-		assertNotNull("Response is null",response.getStatusCode());
+		assertNotNull("Response is null", response);
+		assertNotNull("Response is null", response.getStatusCode());
 		assertEquals("HTTPCode isn't created", response.getStatusCode(), HttpStatus.CREATED);
-		
+
 	}
-	
+
 	@Test
-	public void givenOwnerIdAndOthercharIdAndRelationThenSaveIt(){
+	public void givenOwnerIdAndOthercharIdAndRelationThenSaveIt() {
 		String ownerName = "Owner Name";
 		String otherName = "Other name";
 		Date date = new Date();
 		Relation relation = new Relation();
 		relation.setDate(date);
 
-		Mockito.when(relationService.save(ownerName, otherName,relation)).thenReturn(relation);
-		ResponseEntity response = relationController.save(ownerName,otherName,relation);
+		Mockito.when(relationService.save(ownerName, otherName, relation)).thenReturn(relation);
+		ResponseEntity response = relationController.save(ownerName, otherName, date, relation);
 
-		assertNotNull("Response is null",response);
-		assertNotNull("Response is null",response.getStatusCode());
-		assertEquals("HTTPCode isn't created", response.getStatusCode(), HttpStatus.CREATED);
-		
+		assertNotNull("Response is null", response);
+		assertNotNull("Response is null", response.getStatusCode());
+		assertEquals("HTTPCode isn't created", response.getStatusCode(), HttpStatus.ACCEPTED);
+
 	}
 
 }
